82bde0948e2922ec7d7bb6b739b4d547974c9e4b
0ranko0P <ranko0p@outlook.com>
2019-10-29 00:02:02 +0800

    msm: mdss: dsi: Fix missing display state api for maple
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -45,6 +45,8 @@
 #include "mdss_dba_utils.h"
 #include "mdss_debug.h"
 
+#include <linux/display_state.h>
+
 #define DT_CMD_HDR 6
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
@@ -74,7 +76,11 @@ extern struct msm_fb_data_type *mfd_priv;
 	return 0;
 }
 #endif
-
+bool display_on = true;
+bool is_display_on()
+{
+	return display_on;
+}
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
@@ -1221,6 +1227,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1239,6 +1247,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dba_utils_video_off(pinfo->dba_data);
 		mdss_dba_utils_hdcp_enable(pinfo->dba_data, false);
 	}
+
+	display_on = false;
 
 end:
 	pr_debug("%s:-\n", __func__);
